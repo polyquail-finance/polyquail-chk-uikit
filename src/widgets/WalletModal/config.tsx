e60afe7162cc1f0fadff99b5ee2f7f5dbcbd860f
polyquail-finance <polyquail.finance@gmail.com>
2021-09-28 10:34:36 +0800

    mathwallet

--- src/widgets/WalletModal/config.tsx
+++ src/widgets/WalletModal/config.tsx
@@ -1,15 +1,20 @@
 import Metamask from "./icons/Metamask";
 import MathWallet from "./icons/MathWallet";
-import TokenPocket from "./icons/TokenPocket";
-import TrustWallet from "./icons/TrustWallet";
-import WalletConnect from "./icons/WalletConnect";
-import BinanceChain from "./icons/BinanceChain";
+// import TokenPocket from "./icons/TokenPocket";
+// import TrustWallet from "./icons/TrustWallet";
+// import WalletConnect from "./icons/WalletConnect";
+// import BinanceChain from "./icons/BinanceChain";
 import { Config } from "./types";
 
 const connectors: Config[] = [
   {
     title: "Metamask",
     icon: Metamask,
+    connectorId: "injected",
+  },
+  {
+    title: "MathWallet",
+    icon: MathWallet,
     connectorId: "injected",
   },
 ];
